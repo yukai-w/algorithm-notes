75256c6bd8af7e8ed146e15541049c88b3369309
yukai-w <yukai.w@outlook.com>
2019-04-23 18:28:16 +0800

    feat 无重复字符的最长子串

---
+++ topics/20190423/max_length.js
@@ -0,0 +1,48 @@
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  var list = s.split("");
+  var max = 0;
+
+  list.reduce(reducer, []);
+
+  function reducer(reduceList, item) {
+    // 查找是否存在
+    var index = reduceList.indexOf(item);
+    if (index === -1) {
+      // 不存在push进reduceList
+      reduceList.push(item);
+    } else {
+      // 存在的话从存在的地方截取
+      reduceList = reduceList.slice(index + 1);
+      reduceList.push(item);
+    }
+    max = Math.max(max, reduceList.length);
+    return reduceList;
+  }
+
+  return max;
+};
